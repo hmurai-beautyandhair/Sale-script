c58af795fc9cfadc46ca40a456775a18bc18df45
Hanna Murai <hannamurai@Hannas-MBP.lan1>
2020-12-07 20:32:38 -0600

    polaris

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,24 +13,33 @@
 
 
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '@shopify/polaris/dist/styles.css';
-import "./index.css";
-import "./styles/style.css";
-import {AppProvider} from '@shopify/polaris';
-import enTranslations from '@shopify/polaris/locales/en.json';
-import "bootstrap/dist/css/bootstrap.min.css";
-import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import '@shopify/polaris/dist/styles.css';
+// import "./index.css";
+// import "./styles/style.css";
+// import {AppProvider} from '@shopify/polaris';
+// import enTranslations from '@shopify/polaris/locales/en.json';
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
 
-import App from './App';
+// import App from './App';
 
-function WrappedApp() {
-  return (
-    <AppProvider i18n={enTranslations}>
-      <App />
-    </AppProvider>
-  );
-}
+// function WrappedApp() {
+//   return (
+//     <AppProvider i18n={enTranslations}>
+//       <App />
+//     </AppProvider>
+//   );
+// }
 
-ReactDOM.render(<WrappedApp />, document.getElementById('root'));
+// ReactDOM.render(<WrappedApp />, document.getElementById('root'));
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+import App from "./App";
+import "@shopify/polaris/dist/styles.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
