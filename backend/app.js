8d7c8e65730e164fdf4eb2856ce852a2a2a3e1d9
Hanna Murai <hannamurai@Hannas-MacBook-Pro.local>
2020-12-21 16:00:47 -0600

    v

--- backend/app.js
+++ backend/app.js
@@ -80,14 +80,14 @@ app.get("*", (req, res) => {
 });
 
 
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
 
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
+//   app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+//   });
+// }
 
 
 module.exports = app;
