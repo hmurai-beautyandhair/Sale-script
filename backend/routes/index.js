6feef6cd5b5301c699912d6b06c368fb6335b4bf
Hanna Murai <hannamurai@Hannas-MBP.lan1>
2020-12-21 20:46:46 -0600

    c

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -382,11 +382,15 @@ router.get("/recent-links/:id", (req, res, next) => {
     if(result) {
       if(result.length > 0) {
         if(links in result[0] ) {
+          if(result[0]?.links) {
+            if(result[0]?.links.length > 0) {
     result[0]?.links?.map(x =>{
       arr.push(Object.values(x))
     })
     let send = arr.sort((a, b) => b[0]?.index - a[0]?.index).slice(0, 5)
     res.json(result[0].links);
+  }
+  }
   }
 }
   }
